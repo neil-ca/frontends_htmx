25c2a86696bbfbceb2afaae29d31abbaf432c05c
ulises <ulisescamps9@gmail.com>
2020-10-30 16:58:50 -0600

    create post

--- posts-app/src/main/java/com/ulicode/posts/app/PostDAO.java
+++ posts-app/src/main/java/com/ulicode/posts/app/PostDAO.java
@@ -1,12 +1,32 @@
-
 package com.ulicode.posts.app;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 /**
  *
- * @author erick
+ * @author ulises
  */
 public class PostDAO {
     public static void createPost(Post post) {
+        Connect db_connect = new Connect();
+        
+        try(Connection connecttion = db_connect.get_connection()) {
+            PreparedStatement ps = null;
+            try {
+                String query="INSERT INTO posts (post, author_post) VALUES (?, ?);"; 
+                ps = connecttion.prepareStatement(query);
+                ps.setString(1, post.getPost());
+                ps.setString(2, post.getAuthor_post());
+                ps.executeUpdate();
+                System.out.println("post created");
+            }catch(SQLException ex) {
+                System.out.println(ex);
+            }
+        }catch(SQLException e) {
+            System.out.println(e);
+        }
         
     }
     
--- posts-app/src/main/java/com/ulicode/posts/app/postService.java
+++ posts-app/src/main/java/com/ulicode/posts/app/postService.java
@@ -1,13 +1,25 @@
 
 package com.ulicode.posts.app;
 
+import java.util.Scanner;
+
 /**
  *
  * @author erick
  */
 public class postService {
     public static void createPost() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Write your post");
+        String post = sc.nextLine();
         
+        System.out.println("Author: ");
+        String author = sc.nextLine();
+        
+        Post register = new Post();
+        register.setPost(post);
+        register.setAuthor_post(author);
+        PostDAO.createPost(register);
     }
     public static void getPosts() {
         
