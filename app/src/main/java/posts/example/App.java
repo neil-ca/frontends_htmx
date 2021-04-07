9fc9609759fe25f244669e15c9503eeac1f11123
ulises <ulisescamps9@gmail.com>
2021-04-06 22:22:02 -0500

    change to gradle
    Signed-off-by: ulises <ulisescamps9@gmail.com>

---
+++ app/src/main/java/posts/example/App.java
@@ -0,0 +1,14 @@
+package posts.example;
+
+// import java.util.Scanner;
+
+/** 
+ * @author Neil
+ * */
+public class App {
+    public static void main(String[] args) {
+        int a = 0;
+        System.out.println("tect");
+        System.out.println(a);
+    }
+}
