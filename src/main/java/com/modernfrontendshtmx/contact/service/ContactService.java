d1c74ced51018e06bfe9f58a59c2d6fc6bd5bf4d
neil-ca <ulisescamps9@gmail.com>
2023-12-23 23:19:49 -0600

    Click to load more

--- src/main/java/com/modernfrontendshtmx/contact/service/ContactService.java
+++ src/main/java/com/modernfrontendshtmx/contact/service/ContactService.java
@@ -4,6 +4,7 @@ import com.modernfrontendshtmx.contact.Contact;
 import com.modernfrontendshtmx.contact.ContactId;
 import com.modernfrontendshtmx.contact.ContactNotFoundException;
 import com.modernfrontendshtmx.contact.repository.ContactRepository;
+import com.modernfrontendshtmx.contact.repository.Page;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,42 +12,47 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class ContactService {
-  private final ContactRepository repository;
+    private final ContactRepository repository;
 
-  public List<Contact> getAll() { return repository.findAll(); }
+    public List<Contact> getAll() {
+        return repository.findAll();
+    }
 
-  public Contact storeNewContact(String givenName, String familyName,
-                                 String phone, String email) {
-    Contact contact =
-        new Contact(repository.nextId(), givenName, familyName, phone, email);
-    repository.save(contact);
-    return contact;
-  }
+    public Contact storeNewContact(String givenName, String familyName,
+            String phone, String email) {
+        Contact contact = new Contact(repository.nextId(), givenName, familyName, phone, email);
+        repository.save(contact);
+        return contact;
+    }
 
-  public List<Contact> searchContacts(String query) {
-    return repository.findAllWithNameContaining(query);
-  }
+    public List<Contact> searchContacts(String query) {
+        return repository.findAllWithNameContaining(query);
+    }
 
-  public Contact getContact(ContactId contactId) {
-    return repository.findById(contactId).orElseThrow(
-        () -> new ContactNotFoundException(contactId));
-  }
+    public Contact getContact(ContactId contactId) {
+        return repository.findById(contactId).orElseThrow(
+                () -> new ContactNotFoundException(contactId));
+    }
 
-  public void updateContact(ContactId contactId, String givenName,
-                            String familyName, String phone, String email) {
-    Contact contact = getContact(contactId);
-    contact.setGivenName(givenName);
-    contact.setFamilyName(familyName);
-    contact.setPhone(phone);
-    contact.setEmail(email);
-    repository.save(contact);
-  }
+    public void updateContact(ContactId contactId, String givenName,
+            String familyName, String phone, String email) {
+        Contact contact = getContact(contactId);
+        contact.setGivenName(givenName);
+        contact.setFamilyName(familyName);
+        contact.setPhone(phone);
+        contact.setEmail(email);
+        repository.save(contact);
+    }
 
-  public void deleteContact(ContactId contactId) {
-    repository.deleteById(contactId);
-  }
+    public void deleteContact(ContactId contactId) {
+        repository.deleteById(contactId);
+    }
 
-  public boolean contactWithEmailExists(String email) {
-      return repository.existsByEmail(email);
-  }
+    public boolean contactWithEmailExists(String email) {
+        return repository.existsByEmail(email);
+    }
+
+    public Page<Contact> getAll(int page) {
+        return repository.findAllOrderedByName(page, 10);
+    }
 }
