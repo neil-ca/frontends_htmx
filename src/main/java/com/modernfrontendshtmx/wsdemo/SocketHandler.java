57d9176a7b2bd6695ddd691dc80f4a1e0aaae55d
neil-ca <ulisescamps9@gmail.com>
2023-12-28 14:20:37 -0600

    Send HTML back using thymeleaf templates

--- src/main/java/com/modernfrontendshtmx/wsdemo/SocketHandler.java
+++ src/main/java/com/modernfrontendshtmx/wsdemo/SocketHandler.java
@@ -4,15 +4,17 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.CopyOnWriteArrayList;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.asm.TypeReference;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring6.SpringTemplateEngine;
 
 @Component
 public class SocketHandler extends TextWebSocketHandler {
@@ -20,19 +22,22 @@ public class SocketHandler extends TextWebSocketHandler {
     private final List<WebSocketSession> sessions = new CopyOnWriteArrayList<>();
 
     private final ObjectMapper objectMapper;
+    private final SpringTemplateEngine templateEngine;
 
-    public SocketHandler(ObjectMapper objectMapper) {
+    public SocketHandler(ObjectMapper objectMapper,
+            SpringTemplateEngine templateEngine) {
         this.objectMapper = objectMapper;
+        this.templateEngine = templateEngine;
     }
 
     @Override
     public void handleTextMessage(WebSocketSession session, TextMessage message)
             throws IOException {
-        Map<String, Object> value = objectMapper.readValue(message.getPayload(), new TypeReference<>() {
+        Map<String, Object> value = objectMapper.readValue(message.getPayload(), new com.fasterxml.jackson.core.type.TypeReference<>() {
 
         });
         String userMessage = (String) value.get("chatMessage");
-        sendMessageToWebSocketsessions(userMessage);
+        sendMessageToWebSocketsessions(session, userMessage);
     }
 
     @Override
@@ -41,19 +46,38 @@ public class SocketHandler extends TextWebSocketHandler {
     }
 
     @Override
-    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
-        sessions.remove(session); 
+    public void afterConnectionClosed(WebSocketSession session,
+            CloseStatus status) throws Exception {
+        sessions.remove(session);
     }
 
-    private void sendMessageToWebSocketsessions(String message) {
+    private void sendMessageToWebSocketsessions(WebSocketSession currentSession,
+            String message) {
         for (WebSocketSession webSocketSession : sessions) {
             try {
-                if (webSocketSession.isOpen()) {
-                    webSocketSession.sendMessage(new TextMessage(message));
+                if (!webSocketSession.isOpen()) {
+                    continue;
+                }
+                if (webSocketSession.equals(currentSession)) {
+                    webSocketSession.sendMessage(
+                            new TextMessage(userMessageHtml(message)));
+                } else {
+                    webSocketSession.sendMessage(
+                            new TextMessage(incomingMessageHtml(message)));
                 }
             } catch (IOException e) {
                 LOGGER.debug("Unable to send message to {}", webSocketSession);
             }
         }
+    }
+
+    private String userMessageHtml(String message) {
+        Context context = new Context(null, Map.of("message", message));
+        return templateEngine.process("websockets", Set.of("user-message"), context);
+    }
+
+    private String incomingMessageHtml(String message) {
+        Context context = new Context(null, Map.of("message", message));
+        return templateEngine.process("websockets", Set.of("incoming-message"), context);
     }
 }
